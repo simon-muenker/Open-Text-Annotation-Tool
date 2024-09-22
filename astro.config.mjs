1ac2c3faa28ccf63fd40530e77d92950c2fe631e
Simon Münker <muenker@uni-trier.de>
2024-09-22 16:16:26 +0200

    initial content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import svelte from '@astrojs/svelte';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), svelte(), icon()]
+});
