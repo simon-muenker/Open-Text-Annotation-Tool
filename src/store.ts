bdc6ce153ef5b99ec67fddd6f5035796db942877
Simon Münker <muenker@uni-trier.de>
2024-09-24 09:23:30 +0200

    move store collation to store file

--- src/store.ts
+++ src/store.ts
@@ -5,3 +5,21 @@ export const store: Record<string, PreinitializedMapStore> = {
   topic: map<Record<string, string>>({}),
   sentiment: map<Record<string, string>>({}),
 };
+
+
+export const getCollatedStore = (): Record<string, any> => {
+  let output: Record<string, any> = {
+    annotation: {},
+  };
+
+  for (const [key, value] of Object.entries(store)) {
+    if (key == "survey") {
+      output[key] = value.value;
+    } else {
+      output["annotation"][key] = value.value;
+    }
+  }
+
+  console.debug(output);
+  return output;
+}
