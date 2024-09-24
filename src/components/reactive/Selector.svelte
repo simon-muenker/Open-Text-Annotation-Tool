6f9466c99a3ce76c7a2ae910dfc4d1d69c85d921
Simon Münker <muenker@uni-trier.de>
2024-09-24 17:11:15 +0200

    enhance selection ux

--- src/components/reactive/Selector.svelte
+++ src/components/reactive/Selector.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import {store} from "@/store.ts";
+  import { store } from "@/store.ts";
 
-	export let id: string;
-  export let options:Array<string>;
+  export let id: string;
+  export let options: Array<string>;
   export let storeItem: string;
 
   let selection: string = options[0];
@@ -10,19 +10,28 @@
   store[storeItem].setKey(id, selection);
   const onChange = () => {
     store[storeItem].setKey(id, selection);
-	}
+  };
 </script>
 
-<select id={storeItem + id} bind:value={selection} on:change={onChange} class="inline-flex cursor-pointer select-none appearance-none w-auto rounded-lg py-2 pl-3 pr-10 text-left shadow-md">
+<select
+  id={storeItem + id}
+  bind:value={selection}
+  on:change={onChange}
+  class="
+    inline-flex cursor-pointer select-none appearance-none w-auto
+    rounded-lg py-2 pl-3 pr-10 text-left shadow-md border-2
+    {selection != options[0] ? 'border-emerald-500' : 'border-transparent'}
+  "
+>
   {#each options as option}
     <option value={option}>{option}</option>
   {/each}
 </select>
-  
+
 <style>
   select {
     background-image: linear-gradient(45deg, transparent 50%, currentColor 50%),
-    linear-gradient(135deg, currentColor 50%, transparent 50%);
+      linear-gradient(135deg, currentColor 50%, transparent 50%);
     background-position:
       calc(100% - 20px) calc(1px + 50%),
       calc(100% - 16.1px) calc(1px + 50%);
